a0a62ac7b01b44ec4747d36b5a1260af7da9a6ed
JTomI <36204969+JTomI@users.noreply.github.com>
2022-05-09 18:51:05 -0400

    refined RVM scripts

--- AMF_LSPone/Rotary_Valve/RVM_tools.py
+++ AMF_LSPone/Rotary_Valve/RVM_tools.py
@@ -16,30 +16,24 @@ import time
 class RVM(object):
 	def __init__(self,port='COM3'):
 		self.port=port
+		self.current_valve=0
 		# Open serial connection -> check COM port on your device
-		self.rvm = Serial(port, 9600, timeout=1000)
+		print("RVM initializing")
+		self.rvm = Serial(port, 9600, timeout=10)
 		print('RVM connected on',self.rvm.name)
-		self.rvm.write(b"/1?6R\r")
-		msg = self.rvm.readline()
-		print("RVM initialised. Current valve = "+str(msg))
+		# Close the connection until it is needed again
 		self.rvm.close()
-		# sys.exit(0)
 
-	def query(self, cmd='?6', rbytes=100):
+	def cmd(self, msg=''):
+		# Send arbitrary command to RVM. Consult AMF manual for the RVM commands.
+		# /1 	- command start
+		# {} 	- command body
+		# R 	- command end
+		# \r    - carriage return caracter
 		self.rvm.open()
-		cmd_str = "/1{}R\r".format(cmd)
+		cmd_str = "/1{}R\r".format(msg)
 		self.rvm.write(cmd_str.encode('utf-8'))
-		msg = self.rvm.readline()
 		self.rvm.close()
-		return msg
-
-	def get_ID(self):
-		self.rvm.open()
-		self.rvm.write(b"/1?9000R\r")
-		id_ = self.rvm.readline()
-		print("RVM unique ID = ",id_)
-		self.rvm.close()
-		return id_
 
 	def reset(self):
 		#Reset connection
@@ -52,40 +46,36 @@ class RVM(object):
 		self.rvm.open()
 		self.rvm.write(b"/1ZR\r")
 		time.sleep(5)
-		valv_num=self.rvm.write(b"/1?26R\r")
-		print("Homing complete. Current valve = "+str(valv_num))
+		print("Homing complete.")
 		self.rvm.close()
-		return valv_num
 
 	def move(self,valve=None):
 		# Moves to new valve position (1,2,3,4,5,6) in shortest path.
 		# /1 	- command start
-		# b3 	- go to valve port 3, shortest path, unless already at valve 3
+		# b{} 	- move to valve port {} by the shortest path, unless already at that valve.
 		# R 	- command end
+		# \r    - carriage return caracter
 		if valve == None:
 			print("No valve (1,2,3,4,5,6) selected")
 		else:
 			self.rvm.open()
 			cmd_str = "/1b{}R\r".format(int(valve))
-			rvm.write(cmd_str.encode('utf-8'))
-			time.sleep(10)
-			valv_num=self.rvm.write(b"/1?26R\r")
-			print("Move complete. Current valve = "+str(valv_num))
+			self.rvm.write(cmd_str.encode('utf-8'))
+			self.current_valve=valve
+			time.sleep(5)
+			print("Move complete. Current valve = "+str(self.current_valve))
 			self.rvm.close()
-			return valv_num
-
-	def exit(self):
-		#Dramatic program end
-		sys.exit(0)
 
 
 if __name__ == '__main__':
 	# Example connection and use. 
 	rvm1 = RVM()
-	# rvm1.get_ID()
-	print(rvm1.query())
-	# rvm1.reset()
-	# rvm1.move(valve=1)
-
-	#Make sure to close connection!
+	rvm1.rehome()
+	print(rvm1.current_valve)
+	rvm1.move(valve=1)
+	print(rvm1.current_valve)
+	rvm1.move(valve=6)
+	print(rvm1.current_valve)
+	rvm1.reset()
+	#Make doubly that connection is closed
 	sys.exit(0)
